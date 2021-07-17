8eac6dc16c93e7c57fd281de05dc3af6eb043292
jpvalenzuelab <jpvalenzuelab@uqvirtual.edu.co>
2021-07-17 12:42:14 -0500

    Testconexion

--- Proyecto/db.php
+++ Proyecto/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-$Conexion= mysql_connect("localhost" ,"root", "", "nombre de base de datos");
+$conexion= mysql_connect("localhost" ,"root", "1234", "bd_industrias_4");
 
 echo "Conectado"
 
--- Proyecto/validar.php
+++ Proyecto/validar.php
@@ -3,4 +3,28 @@
 $usuario=$_POST['usuario'];
 $contraseña=$_POST['contraseña'];
 session_start();
-?>
+
+include('db.php');
+
+$consulta = "SELECT*FROM usuario where correo ='$usuario' and contraseña= '$contraseña';";
+$resultado = mysqli_query($conexion, $consulta);
+
+$filas = mysqli_num_rows($resultado);
+if($filas)
+{
+    header("location:home.php");
+}
+else
+{
+
+    ?>
+    <?php
+
+    include("login.php");
+    ?>
+    <h1 class = "bad">Error en la autenticacion </h1>
+    <?php
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conexion;)
