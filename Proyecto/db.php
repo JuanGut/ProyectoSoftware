b6779241ce625b23db4ec19d5d722241db0e46fe
jpvalenzuelab <jpvalenzuelab@uqvirtual.edu.co>
2021-07-17 15:14:35 -0500

    Conexion bd nube

--- Proyecto/db.php
+++ Proyecto/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conexion= mysqli_connect("localhost" ,"root", "", "bd_industrias_4");
+$conexion= mysqli_connect("sql307.epizy.com" ,"epiz_29169643", "CVT90zFcgH", "epiz_29169643_bd_industrias_4");
 
 echo "Conectado"
 
